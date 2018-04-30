7ecfa1bce6ce08b8ea4dc273379aec9d13578fcd
ahmetacer <dev>
2018-04-30 12:48:56 +0300

    commit for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,99 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { AuthGuard } from './services/AuthGuard/auth.guard';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatStepperModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from '@angular/material';
+import { routing } from './app.routing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationService } from './services/authentication';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './services/api';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    routing,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    HttpModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthGuard, AuthenticationService, ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/authentication.ts
+++ src/app/services/authentication.ts
@@ -1,24 +1,30 @@
-import {Injectable} from '@angular/core';
-import {Http, Headers, Response, RequestOptions, ResponseContentType} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { environment } from '../../../environments/environment';
-import {CustomURLSearchParams} from '../CustomURLSearchParams';
-import { GlobalVairables } from '../GlobalVairables';
+import { environment } from '../../environments/environment';
+import { CustomURLSearchParams } from './CustomURLSearchParams';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable()
 export class AuthenticationService {
+  apihost = environment.apihost;
+  apibase = '/auth';
 
   constructor(private http: Http) {
   }
 
-  apihost = environment.apihost;
-  apibase = '/auth';
 
-  login(identityno: string, password: string) {
+  encryptPw(value) {
+    const key = CryptoJS.enc.Base64.parse('37KvDCAC11CCXLOKSX4CvjYOh9Y');
+    const iv = CryptoJS.enc.Base64.parse('#base64IV#');
+    const encryptedpw = CryptoJS.AES.encrypt(value, key, {iv: iv});
+    return encryptedpw.toString();
+  }
+
+  login(username: string, password: string) {
     const body = new CustomURLSearchParams();
-    body.set('identityno', identityno);
-    body.set('password', GlobalVairables.encryptPw(password));
+    body.set('username', username);
+    body.set('password', this.encryptPw(password));
 
     const headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded');
@@ -31,95 +37,6 @@ export class AuthenticationService {
     // remove user from local storage to log user out
     sessionStorage.removeItem('SESSIONID');
   }
-
-  userexist(_identityno: string) {
-
-    const body = new CustomURLSearchParams();
-    body.set('identityno', _identityno);
-
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-
-    return this.http.post(this.apihost + this.apibase + '/userexist', body.toString(), {headers: headers})
-      .map((response: Response) => response.json());
-  }
-
-  getSignupToken(identityno: string) {
-    const body = new CustomURLSearchParams();
-    body.set('identityno', identityno);
-
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-
-
-    return this.http.post(this.apihost + this.apibase + '/signupredirect', body.toString(), {headers: headers})
-      .map((response: Response) => response.json());
-  }
-
-  getCheckSignupToken(signuptoken: string) {
-    const body = new CustomURLSearchParams();
-    body.set('signuptoken', signuptoken);
-
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-
-
-    return this.http.post(this.apihost + this.apibase + '/checksignupredirect', body.toString(), {headers: headers})
-      .map((response: Response) => response.json());
-  }
-
-  yonlendir_check(yonlendirtoken: string) {
-    const body = new CustomURLSearchParams();
-    body.set('yonlendirtoken', yonlendirtoken);
-
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-
-
-    return this.http.post(this.apihost + this.apibase + '/yonlendir_check', body.toString(), {headers: headers})
-      .map((response: Response) => response.json());
-  }
-
-
-  // apiRoutes.post('/signupandauthenticate', function (req, res) {
-  //
-  // if (req.body.name && req.body.surname && req.body.email && req.body.password && req.body.identityno) {
-
-
-  // signupandauthenticate(user: KayitModel) {
-  //
-  //   const body = new CustomURLSearchParams();
-  //   body.set('name', user.adsoyad);
-  //   body.set('email', user.email);
-  //   body.set('phone', user.telefon);
-  //
-  //   const key = CryptoJS.enc.Base64.parse(this.privateKey);
-  //   const iv  = CryptoJS.enc.Base64.parse('#base64IV#');
-  //   const encryptedpw = CryptoJS.AES.encrypt(user.password, key, {iv: iv});
-  //
-  //
-  //   body.set('password', encryptedpw.toString());
-  //   body.set('identityno', user.tcno);
-  //
-  //   const headers = new Headers();
-  //   headers.append('Content-Type', 'application/x-www-form-urlencoded');
-  //
-  //   return this.http.post(this.apihost + this.apibase + '/signupandauthenticate', body.toString(), {headers: headers})
-  //     .map((response: Response) => response.json());
-  // }
-
-  test_auto_auth(identityno: string) {
-    const body = new CustomURLSearchParams();
-    body.set('identityno', identityno);
-
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-
-
-    return this.http.post(this.apihost + this.apibase + '/test_auto_auth', body.toString(), {headers: headers})
-      .map((response: Response) => response.json());
-  }
-
 
 }
 
